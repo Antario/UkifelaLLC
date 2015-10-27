0cc4c68cef92e6b8af599f2a7234b6be446dd922
Antario <dimasya007@gmail.com>
2015-10-27 11:29:27 +0200

    Initial commit

---
+++ public_html/wp-content/themes/advertica-lite/includes/front-parallax-section.php
@@ -0,0 +1,9 @@
+<div id="full-division-box" class="skt-section">
+	<div class="container full-content-box" >
+		<div class="row-fluid">
+			<div class="span12">
+			<?php echo do_shortcode( get_theme_mod('parallax_content', '<div class="skt-awesome-section"><div class="skt-awesome-title">'.__('Our partners').'</div><div class="skt-awesome-desp">'.__('').'</div></div>') ); ?>
+			</div>
+		</div>
+	</div>
+</div>
